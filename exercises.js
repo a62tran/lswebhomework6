97276ea767c491a4c887827cc2e7ca605e4b5143
Aaron Tran <A62tran@Aarons-MacBook-Air.local>
2017-06-15 21:46:13 -0400

    updated

--- exercises.js
+++ exercises.js
@@ -13,37 +13,38 @@ function subtractFive(num) {
 function areSameLength(str1, str2) {
   //return true if the two strings have the same length
   //otherwise return false
-  if (str1.length === str2.length)
-    {return true}
+  if (str1.length == str2.length)
+    {return true;}
   else
-    {return false}
+    {return false;}
 }
 
 function areEqual(x, y) {
   //return true if x and y are the same
   //otherwise return false
-    if (x === y)
-    {return true}
-  else
-    {return false}
+    if (x == y) {
+      return true;
+    } else {
+      return false;
+    }
 }
 
 function lessThanNinety(num) {
   //return true if num is less than ninety
   //otherwise return false
-    if (num > 90)
-    {return true}
+    if (num < 90)
+    {return true;}
   else
-    {return false}
+    {return false;}
 }
 
 function greaterThanFifty(num) {
   //return true if num is greater than fifty
   //otherwise return false
       if (num > 50)
-    {return true}
+    {return true;}
   else
-    {return false}
+    {return false;}
 }
 
 function add(x, y) {
@@ -74,20 +75,19 @@ function getRemainder(x, y) {
 function isEven(num) {
   //return true if num is even
   //otherwise return false
-  if (num%2 == 0)
-    {return true}
+  if (num%2 === 0)
+    {return true;}
   else
-    {return false}
+    {return false;}
 }
 
 function isOdd(num) {
   //return true if num is odd
   //otherwise return false
-  if (num%2 != 0)
-    {return true}
+  if (num%2 === 0)
+    {return false;}
   else
-    {return false}
-}
+    {return true;}
 }
 
 function square(num) {
@@ -118,19 +118,19 @@ function roundUp(num) {
 function addExclamationPoint(str) {
   //add an exclamation point to the end of str and return the new string
   //'hello world' -> 'hello world!'
-  return str + "!";
+  return str + '!';
 }
 
 function combineNames(firstName, lastName) {
   //return firstName and lastName combined as one string and separated by a space.
   //'Lambda', 'School' -> 'Lambda School'
-  return firstName + " " + lastName;
+  return firstName + ' ' + lastName;
 }
 
 function getGreeting(name) {
   //Take the name string and concatenate other strings onto it so it takes the following form:
   //'Sam' -> 'Hello Sam!'
-  return "Hello " + name + "!";
+  return 'Hello ' + name + '!';
 }
 
 //If you can't remember these area formulas then head over to Google or look at the test code.
